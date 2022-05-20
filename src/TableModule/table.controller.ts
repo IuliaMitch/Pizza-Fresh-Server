08e717a1359e1dabbdb735312103ed8192739581
IuliaMitch <noipasz@gmail.com>
2022-05-19 23:00:23 -0300

    feat(table): add update endpoint

--- src/TableModule/table.controller.ts
+++ src/TableModule/table.controller.ts
@@ -2,11 +2,12 @@
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Table } from './entities/table.entity';
 import { CreateTableDto } from './DTO/create-table.dto';
 import { TableService } from './table.service';
+import { UpdateTableDto } from './DTO/update-table.dto';
 
 @ApiTags('table')
 @Controller('table')
@@ -23,7 +24,7 @@ export class TableController {
 
   @Get(':id')
   @ApiOperation({
-    summary: 'Visualizar uma mesa'
+    summary: 'Visualizar uma mesa por Id'
   })
   findOne(@Param('id') id: string): Promise<Table> {
     return this.tableService.findOne(id)
@@ -36,4 +37,12 @@ export class TableController {
   create(@Body() dto: CreateTableDto): Promise<Table> {
     return this.tableService.create(dto);
   }
+
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'Editar uma mesa por ID'
+  })
+  update(@Param('id') id: string, @Body() dto: UpdateTableDto): Promise<Table> {
+    return this.tableService.update(id, dto)
+  } 
 }
--- src/TableModule/table.service.ts
+++ src/TableModule/table.service.ts
@@ -5,10 +5,12 @@ https://docs.nestjs.com/providers#services
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateTableDto } from './DTO/create-table.dto';
+import { UpdateTableDto } from './DTO/update-table.dto';
 import { Table } from './entities/table.entity';
 
 @Injectable()
 export class TableService {
+
   tables: Table[] = [];
 
   constructor(private readonly prisma: PrismaService) {}
@@ -29,5 +31,17 @@ export class TableService {
     const data: Table = { ...dto };
 
     return this.prisma.table.create({ data });
+  }
+
+  update(id: string, dto: UpdateTableDto): Promise<Table> {
+    
+    const data: Partial<Table> = {...dto}
+
+    this.prisma.table.update({
+      where: {
+        id
+      },
+      data,
+    })
   }
 }
