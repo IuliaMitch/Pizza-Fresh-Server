23e2367352f34b756d129397c9f321e41602c3b6
IuliaMitch <noipasz@gmail.com>
2022-05-24 12:41:01 -0300

    feat(user): implement dto & entity

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,45 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsUrl, Matches, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    @ApiProperty({
+        description: 'Nome do Usuário, Deve ser unico',
+        example: 'iuliadb',
+
+    })
+    nickname: string;
+    @IsString()
+    @ApiProperty({
+        description: 'Nome do Usuário. Apenas para exibição',
+        example: 'Iulia Mitch',
+
+    })
+    name: string;
+    @IsString()
+    @MinLength(6)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'Senha muito fraca',
+      })
+    @ApiProperty({
+        description: 'Senha do usuário para login',
+        example: 'Abcd@1234',
+
+    })
+    password: string;
+    @IsString()
+    @ApiProperty({
+        description: 'A confirmação da senha deve ser igual a senha',
+        example: 'Abcd@1234',
+
+    })
+    confirmPassword: string;
+    @IsUrl()
+    @ApiProperty({
+        description: 'Imagem de perfil do usuário',
+        example: 'https://avatars.githubusercontent.com/u/85199887?v=4',
+
+    })
+    image: string;
+}
+
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,11 @@
-export class User {}
+export class User {
+    
+    id?: string;
+    nickname: string;
+    name: string;
+    password: string;
+    image: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+      
+}
